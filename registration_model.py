1f15b52e0c0004c2ed7a3faf8689bb4521ac4ecc
GaylouM <gaylord.marville@gmail.com>
2016-08-29 21:33:12 +0200

    Review #2
    changes v2

--- registration_model.py
+++
@@ -1,7 +0,0 @@
-from protorpc import messages
-
-
-class RegisteringForm(messages.Message):
-    """BooleanMessage-- outbound Boolean value message"""
-    registeringStatus = messages.StringField(1)
-    activePlayerName = messages.StringField(2)
